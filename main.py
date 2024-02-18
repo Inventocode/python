09fab504993a5b46e9330ee3605e37b7c7270c67
Inventocode <359148497@qq.com>
2024-02-18 17:19:09 +0800

    更新了亿点点

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from gpiozero import DistanceSensor,LED
 import RPi.GPIO as GPIO
-import time,os
+import time,os,math
 
-# 超声波传感器 BRM
-dis = DistanceSensor(19,26,max_distance=2)
-# 警报灯 BRM
-alert = LED(13)
 # rgb灯 BOARD
 r,g,b = 13,11,15
-# 各灯亮起时间 秒 s
-rt,yt,gt = 10,5,5
+# 各灯亮起时间 秒
+rt,yt,gt = 10,3,7
+# 超声波传感器 BRM
+dis = DistanceSensor(19,26,max_distance=2)
+# 抓拍距离 米
+capture_distance = 0.7
+# 抓拍间隔 秒
+capture_interval = 2
+# 警报灯 BRM
+alert_led = LED(13)
+# 警报声音效
+alert_audio = './media/alert.mp3'
 # 抓拍图片保存路径
 save_path = './image'
-# 抓拍距离 米 m
-capture_distance = 0.3
-# 抓拍间隔 秒 s
-capture_interval = 2
+# 启用语音播报
+enable_tts = True
+# 语音路径
+tts_path = './media/number'
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -28,26 +34,34 @@ def save():
     os.system(ft('fswebcam --title "%Y-%m-%d %H:%M:%S" --no-timestamp -q '+save_path+'/%Y-%m-%d/%H%M%S.jpg'))
 def ft(text):
     return time.strftime(text, time.localtime())
+def sleep(t,end=1):
+    for i in range(t+end-1,end-1,-1):
+        if enable_tts:
+            os.system('mpg123 -q '+tts_path+'/'+str(i)+'.mp3 &')
+        time.sleep(1)
 while True:
     GPIO.output(g,1)
-    time.sleep(gt)
-    for i in range(yt//0.5+1):
+    sleep(gt,4)
+    for i in range(yt,0,-1):
+        if enable_tts:
+            os.system('mpg123 -q '+tts_path+'/'+str(i)+'.mp3 &')
         GPIO.output((r,g),1)
-        time.sleep(0.25)
+        time.sleep(0.5)
         GPIO.output((r,g),0)
-        time.sleep(0.25)
+        time.sleep(0.5)
     GPIO.output(r,1)
     capture_time = time.time()
     end_time = time.time() + rt
     while time.time() < end_time:
-        print(dis.distance)
         if dis.distance < capture_distance:
-            alert.on()
+            alert_led.on()
             if time.time() > capture_time:
                 capture_time = time.time() + capture_interval
                 save()
-                os.system('mpg123 ./alert.mp3')
+                os.system('mpg123 -q media/alert.mp3 &')
         else:
-            alert.off()
-    alert.off()
+            alert_led.off()
+        if (end_time - time.time()) % 1 < 0.001:
+            os.system('mpg123 -q '+tts_path+'/'+str(math.floor(end_time-time.time()+1))+'.mp3 &')
+    alert_led.off()
     GPIO.output(r,0)
